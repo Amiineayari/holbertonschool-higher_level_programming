6db8b6b09b190527a4ca9d593ccd922f836b87a5
Amiineayari <6485@holbertonstudents.com>
2023-06-02 14:10:24 -0700

    This code generates a random number between -10000 and 10000, and then determines the last digit of the number.

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-x = number % 10
-if number < 0:
-                x = -number % 10
-                    x = -x
-                    if x == 0:
-                                    print("Last digit of", number, "is", x, "and is 0")
-                    elif x > 5:
-                                    print("Last digit of", number, "is", x, "and is greater than 5")
+
+if number >= 0:
+                    l_digit = number % 10
+else:
+                    l_digit = number % -10
+
+                    print("Last digit of {} is {}".format(number, l_digit), end='')
+
+                    if l_digit > 5:
+                                        print(" and is greater than 5")
+                    elif l_digit == 0:
+                                        print(" and is 0")
                     else:
-                                    print("Last digit of", number, "is", x, "and is less than 6 and not 0")
-                                    
+                                        print(" and is less than 6 and not 0")
