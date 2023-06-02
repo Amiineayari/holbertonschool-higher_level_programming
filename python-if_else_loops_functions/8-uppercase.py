8251d034e4016aa6b5db7fa5cd2cc0a24adebdff
Amiineayari <6485@holbertonstudents.com>
2023-06-02 14:28:18 -0700

    This code defines a function  that takes a string as input and converts it to uppercase.

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-        for i in range(len(str)):
-                    if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-                                    num = 32
-                    else:
-                                    num = 0
-                                            print("{:c}".format(ord(str[i]) - num), end='')
-                                                print()
+            f = ""
+                for i in range(len(str)):
+                                if (ord(str[i]) in range(97, 123)):
+                                                    f += chr(ord(str[i]) - 32)
+                                else:
+                                                    f += str[i]
+                                                        print("{}".format(f))
