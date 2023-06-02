6086abec118d913f55f08c57592e9ff0e82f6c22
Amiineayari <6485@holbertonstudents.com>
2023-06-02 14:04:33 -0700

    This code generates a random number and determines its last digit. If the number is negative, it calculates the last digit and adjusts its sign accordingly. Then, it checks the value of the last digit and prints a message based on the conditions.

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+x = number % 10
 if number < 0:
-            y = (number * -1) % 10
-                x = -y
-else:
-            x = number % 10
-            if (x > 5):
-                        print(f"Last digit of {number} is {x} and is greater than 5")
-            elif (x == 0):
-                        print(f"Last digit of {number} is {x} and is 0")
-            elif (x < 6):
-                        print(f"Last digit of {number} is {x} and is less than 6 and not 0")
-                        
+                x = -number % 10
+                    x = -x
+                    if x == 0:
+                                    print("Last digit of", number, "is", x, "and is 0")
+                    elif x > 5:
+                                    print("Last digit of", number, "is", x, "and is greater than 5")
+                    else:
+                                    print("Last digit of", number, "is", x, "and is less than 6 and not 0")
+                                    
