00b18a894149c9a59a8e8f40bfecd39946aa74f1
Amiineayari <6485@holbertonstudents.com>
2023-10-19 03:29:59 +0200

    class MyList that inherits from list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-""" module my list"""
-
-
 class MyList(list):
-    """ create a list """
     def print_sorted(self):
-        """ prit list sorted without change the original"""
-        tmp = sorted(self)
-        print(tmp)
-        
+        # Print the list sorted in ascending order
+        print(sorted(self))
+
+# Test the MyList class
+if __name__ == "__main__":
+    my_list = MyList()
+    my_list.append(1)
+    my_list.append(4)
+    my_list.append(2)
+    my_list.append(3)
+    my_list.append(5)
+    print(my_list)
+    
+    # Call the print_sorted method to print the sorted list
+    my_list.print_sorted()
+    
+    print(my_list)
