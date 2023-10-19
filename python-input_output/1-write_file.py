b08ae0b005ca188f11ca8f8e4ef82e103dbb57a0
Amiineayari <6485@holbertonstudents.com>
2023-10-19 05:17:16 +0100

    function that writes a string to a text file (UTF8) and returns the number of characters written

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Start of function write_file"""
+
+
+def write_file(filename="", text=""):
+    """write text in a new file"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        write_data = f.write(text)
+        return write_data
+    
