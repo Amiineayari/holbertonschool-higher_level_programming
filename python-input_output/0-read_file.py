a2d0727648cdfc7e7623fb958d2d4b0bc00b3dbf
Amiineayari <6485@holbertonstudents.com>
2023-10-19 05:13:55 +0100

    class Student that defines a student

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""start of function read_file"""
+
+
+def read_file(filename=""):
+    """read a fle"""
+    with open(filename, encoding="utf-8")as f:
+        read_data = f.read()
+        print(read_data, end="")
+        
